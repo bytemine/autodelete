3671cc40688d39ae5f3c8d85b37379b92dd4f8aa
Daniel Rauer <rauer@bytemine.net>
2018-02-13 11:29:27 +0100

    Do not accept user input with dot or comma.

--- php/class.autodeletemodule.php
+++ php/class.autodeletemodule.php
@@ -105,7 +105,7 @@ class AutodeleteModule extends Module {
 	private function setPeriod($actionData) {
 		$period = $actionData['period'];
     $isPeriodOK = false;
-		if (is_numeric($period) && intval($period) <= PLUGIN_AUTODELETE_MAX_PERIOD && intval($period) > 0) {
+		if (strpos($period, '.') === false && is_numeric($period) && is_int(intval($period)) && intval($period) <= 60 && intval($period) > 0) {
 		  AutodeleteData::setPeriod($period);
 		  $isPeriodOK = true;
 		}
