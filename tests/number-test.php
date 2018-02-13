904e239d92880c9911a8fc17c47190f0489715a3
Daniel Rauer <rauer@bytemine.net>
2018-02-13 12:36:02 +0100

    Global max value for tests.

--- tests/number-test.php
+++ tests/number-test.php
@@ -1,9 +1,11 @@
 <?php
+  global $max_period;
+  $max_period = 60;
   $allowed_input = array("60", "1", "0000000000000000000000000000000005");
   $not_allowed_input = array("0", "34.3", "6,3", "44444444444444444444444444444444", "0xf4c3b00c", "61");
 
   function is_allowed($period) {
-    if (strpos($period, '.') === false && is_numeric($period) && is_int(intval($period)) && intval($period) <= 60 && intval($period) > 0) {
+    if (strpos($period, '.') === false && is_numeric($period) && is_int(intval($period)) && intval($period) <= $GLOBALS["max_period"] && intval($period) > 0) {
 	    return true;
 	  }
 	  return false;
